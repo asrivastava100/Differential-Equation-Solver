a87531910e11d650985043692084f4802dbcbd49
asrivastava100 <48623286+asrivastava100@users.noreply.github.com>
2019-03-16 19:35:51 +0000

    Update init.cpp

--- init.cpp
+++ init.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 int main(){
 
-    int coeff1, coeff2, coeff3;
+    double coeff1, coeff2, coeff3;
 
-    //coeff1,coeff2,coeff3 are the coefficients of f'', f', f respectively
+//coeff1,coeff2,coeff3 are the coefficients of f'', f',f respectively
 
     cout<<"Enter first coefficient: ";
     
@@ -29,24 +29,38 @@ int main(){
 /* Aux equation is coeff1 m^2 + coeff2 m^1 + coeff3 =0. This is a quadratic equation so use quad formula to solve
     */
     
-    int m_1, m_2;
-    m_1 = (-(coeff2) + sqrt(pow(coeff2,2)-4*coeff1*coeff3))/(2*coeff1);
-    
-    m_2 = (-(coeff2) - sqrt(pow(coeff2,2)-4*coeff1*coeff3))/(2*coeff1);
-    
-    cout<<"Root 1 of AUX: "<<m_1<<endl;
-    cout<<"Root 2 of AUX: "<<m_2<<endl;
-    
-    if(m_1==m_2){
-        cout<<"The general solution is y= (A+Bx)e^"<<m_1<<" for some arbitrary constants A and B.\n";
+    double discriminant = (pow(coeff2,2))-4*coeff1*coeff3;
+  
+    if (discriminant<0){
+        
+        double real_part=-coeff2/(2*coeff1);
+        double im_part_1=sqrt(-discriminant)/(2*coeff1);
+        double im_part_2=-sqrt(-discriminant)/(2*coeff1);
+        cout<<"Root 1 of AUX: "<<real_part<<" + "<<im_part_1<<"i"<<endl;
+        cout<<"Root 2 of AUX: "<<real_part<<" + "<<im_part_2<<"i"<<endl;
+        cout<<"The general solution is y= A(e^("<<real_part<<"x))cos("<<abs(im_part_1)<<"x) +B(e^("<<real_part<<"x))\
+        sin("<<abs(im_part_1)<<"x) for some arbitrary constants A and B.\n";
     }
+         
     else{
-        cout<<"The general soltion is y= Ae^"<<m_1<<" + Be^"<<m_2<<" for some arbitrary constants A and B.\n";
+        cout<<"false"<<endl;
+        
+        double m_1, m_2;
+        m_1 = (-(coeff2) + sqrt(pow(coeff2,2)-4*coeff1*coeff3))/(2*coeff1);
+        
+        m_2 = (-(coeff2) - sqrt(pow(coeff2,2)-4*coeff1*coeff3))/(2*coeff1);
+        
+        cout<<"Root 1 of AUX: "<<m_1<<endl;
+        cout<<"Root 2 of AUX: "<<m_2<<endl;
+        
+        if(m_1==m_2){
+            cout<<"The general solution is y= (A+Bx)e^"<<m_1<<" for some arbitrary constants A and B.\n";
+        }
+        else{
+            cout<<"The general solution is y= Ae^"<<m_1<<" + Be^"<<m_2<<" for some arbitrary constants A and B.\n";
+        }
     }
-    
-    
-    
-    
+
     
 
 
