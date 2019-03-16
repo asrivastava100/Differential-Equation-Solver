03019904c915947befd6be1f830af2dd55543f77
asrivastava100 <48623286+asrivastava100@users.noreply.github.com>
2019-03-16 14:17:02 +0000

    Update init.cpp

--- init.cpp
+++ init.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <cmath>
 
+//This programme solves second order homogeneous equations. 
 using namespace std;
 
 int main(){
-
-cout<<"This is a test"<<endl;
-
+int coeff1, coeff2, coeff3  //coeff(i) is the coefficient of f'', f', f   
+cout<<"Enter first coefficient: ";
+cin>>coeff1;
+  
+  
 
 
 }
